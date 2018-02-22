3f97deb4333c54515080e63056a4551031530620
Jordan Crane <jordancrane@protonmail.com>
2018-02-22 13:53:07 -0800

    Initial commit

---
+++ .config/logitech-marble-mouse/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+arg=$1
+
+if [[ -n $arg ]]; then
+    case $arg in
+        "--help" | "-h")
+            echo "Usage: $0 <left-hand|right-hand>"
+            ;;
+        "left-hand")
+            xinput set-prop "Logitech USB Trackball" "libinput Button Scrolling Button" 1
+            xinput set-button-map "Logitech USB Trackball" 3 2 1 4 5 6 7 9 8
+            ;;
+        "right-hand")
+            xinput set-prop "Logitech USB Trackball" "libinput Button Scrolling Button" 3
+            xinput set-button-map "Logitech USB Trackball" 1 2 3 4 5 6 7 8 9
+            ;;
+        *)
+            #Default to right hand config
+            xinput set-prop "Logitech USB Trackball" "libinput Button Scrolling Button" 3
+            xinput set-button-map "Logitech USB Trackball" 1 2 3 4 5 6 7 8 9
+    esac
+else
+    echo "Usage: $0 <left-hand|right-hand>"
+fi
+
+
+
