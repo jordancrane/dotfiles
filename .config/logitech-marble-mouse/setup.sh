b71b363fd6f5f14df90a5958246b1d66c541cba8
Jordan Crane <jordancrane@protonmail.com>
2018-04-12 13:43:58 -0700

    Fix compton issue #477 in i3 config

--- .config/logitech-marble-mouse/setup.sh
+++ .config/logitech-marble-mouse/setup.sh
@@ -9,15 +9,18 @@ if [[ -n $arg ]]; then
             ;;
         "left-hand")
             xinput set-prop "Logitech USB Trackball" "libinput Button Scrolling Button" 1
+            xinput set-prop "Logitech USB Trackball" "libinput Scroll Method Enabled" 0, 0, 1
             xinput set-button-map "Logitech USB Trackball" 3 2 1 4 5 6 7 9 8
             ;;
         "right-hand")
             xinput set-prop "Logitech USB Trackball" "libinput Button Scrolling Button" 3
+            xinput set-prop "Logitech USB Trackball" "libinput Scroll Method Enabled" 0, 0, 1
             xinput set-button-map "Logitech USB Trackball" 1 2 3 4 5 6 7 8 9
             ;;
         *)
             #Default to right hand config
             xinput set-prop "Logitech USB Trackball" "libinput Button Scrolling Button" 3
+            xinput set-prop "Logitech USB Trackball" "libinput Scroll Method Enabled" 0, 0, 1
             xinput set-button-map "Logitech USB Trackball" 1 2 3 4 5 6 7 8 9
     esac
 else
